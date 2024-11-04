38f0c3e2246762f56d78980800847dda8cac5e02
vanshika pal <paln6823@gmail.com>
2024-11-04 15:56:20 +0000

    datatype

---
+++ .github/02_datatype.js
@@ -0,0 +1,17 @@
+let name = "vanshika"
+let age = 56
+let addd =false
+let state;
+
+
+//number => 2to power 67
+//string => ""
+//boolean=> true/false
+//null => standalone value 
+//undefined => not assign
+//symbol => unique
+ 
+//objects
+
+console.log(typeof undefined)//undefined
+console.log(typeof null) //objects
