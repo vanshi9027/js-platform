dd55b4497bb89d91ca572af2bb074e80f2747a7a
vanshika pal <paln6823@gmail.com>
2024-11-04 11:11:00 +0000

    conversionopertion

---
+++ 01_basics/03_conversionopertion.js
@@ -0,0 +1,40 @@
+let number = null
+console.log(typeof number);
+console.log(typeof(number))
+
+let newnumber = Number(number)
+console.log(typeof newnumber)
+console.log(newnumber)
+
+//"33" => 33
+//"33and" => NaN
+//ture => 1/ false => 0
+//null => 0
+//undefined => NaN
+
+let value = Boolean(number)
+console.log(typeof value)
+console.log(value)
+
+
+let Name = String(number)
+console.log(typeof Name)
+console.log(Name)
+
+
+// "" => false
+// "value" => true
+
+
+
+let test = undefined(number)
+console.log(typeof test)
+console.log(test)
+
+//let Name = null(number)
+//console.log(typeof Name)
+//console.log(Name)
+
+
+
+
